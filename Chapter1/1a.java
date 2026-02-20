19076e20f43e79421f0e8cc56e2dd7e0592170b9
ZhargalTor <zhargaltor@gmail.com>
2026-02-20 18:57:27 +0800

    Update 1a.java

--- Chapter1/1a.java
+++ Chapter1/1a.java
@@ -1,1 +1,12 @@
+//1. Приветствовать любого пользователя при вводе его имени через командную строку.
+//Торяшиев Жаргал Б763-2а
 
+void main() {
+    String name;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Введите ваше имя");
+    name = scan.next();
+    System.out.println("Приветствую " + name);
+
+
+}
